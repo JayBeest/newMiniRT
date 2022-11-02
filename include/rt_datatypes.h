67ccb0d9eab63915aeb51801e9361fa1019cc077
jcorneli <jaybart@protonmail.com>
2022-11-02 22:14:17 +0100

    cosmetics

--- include/rt_datatypes.h
+++ include/rt_datatypes.h
@@ -41,9 +41,9 @@ typedef enum e_obj_type
 
 typedef struct s_vector
 {
-	double				x;
-	double				y;
-	double				z;
+	double			x;
+	double			y;
+	double			z;
 }			t_vector;
 
 typedef t_vector t_point;
@@ -51,8 +51,8 @@ typedef t_vector t_color_ratio;
 
 typedef struct s_resolution
 {
-	int					x;
-	int					y;
+	int				x;
+	int				y;
 }			t_resolution;
 
 typedef struct s_canvas_size
@@ -63,18 +63,18 @@ typedef struct s_canvas_size
 
 typedef struct s_color
 {
-	unsigned char 		r;
-	unsigned char 		g;
-	unsigned char 		b;
-	unsigned char 		a;
+	unsigned char 	r;
+	unsigned char 	g;
+	unsigned char 	b;
+	unsigned char 	a;
 }			t_color;
 
 typedef struct s_color_intensity
 {
-	double				r;
-	double				g;
-	double				b;
-	double				a;
+	double			r;
+	double			g;
+	double			b;
+	double			a;
 }			t_color_intensity;
 
 typedef struct s_color_aggregate
@@ -89,9 +89,9 @@ typedef struct s_obj_default
 	t_obj_type		type;
 	t_color			color;
 	t_point			coordinates;
-	float				reflective;
-	float				metal_fuzz;
-	int					specular;
+	float			reflective;
+	float			metal_fuzz;
+	int				specular;
 }			t_obj_default;
 
 typedef struct s_obj_sphere
@@ -99,10 +99,10 @@ typedef struct s_obj_sphere
 	t_obj_type		type;
 	t_color			color;
 	t_point			coordinates;
-	float				reflective;
-	float				metal_fuzz;
-	int					specular;
-	double				radius;
+	float			reflective;
+	float			metal_fuzz;
+	int				specular;
+	double			radius;
 }			t_obj_sphere;
 
 typedef struct s_obj_plane
@@ -110,10 +110,10 @@ typedef struct s_obj_plane
 	t_obj_type		type;
 	t_color			color;
 	t_point			coordinates;
-	float				reflective;
-	float				metal_fuzz;
-	int					specular;
-	t_vector			orientation;
+	float			reflective;
+	float			metal_fuzz;
+	int				specular;
+	t_vector		orientation;
 }			t_obj_plane;
 
 typedef struct s_obj_cylinder
@@ -121,55 +121,54 @@ typedef struct s_obj_cylinder
 	t_obj_type		type;
 	t_color			color;
 	t_point			coordinates;
-	float				reflective;
-	float				metal_fuzz;
-	int					specular;
-	t_vector			orientation;
-	double				radius;
-	double				height;
+	float			reflective;
+	float			metal_fuzz;
+	int				specular;
+	t_vector		orientation;
+	double			radius;
+	double			height;
 }			t_obj_cylinder;
 
 typedef union u_obj_union
 {
 	t_obj_default	def;
-	t_obj_sphere		sphere;
+	t_obj_sphere	sphere;
 	t_obj_plane		plane;
 	t_obj_cylinder	cylinder;
 }			t_obj_union;
 
 typedef struct s_ambient_light
 {
-	double				intensity;
+	double			intensity;
 	t_color			color;
-	bool				set;
+	bool			set;
 }			t_ambient_light;
 
 typedef struct s_spot_light
 {
 	t_point			coordinates;
-	double				intensity;
+	double			intensity;
 	t_color			color;
-	bool				toggle;
+	bool			toggle;
 }			t_spot_light;
 
 typedef struct s_camera
 {
 	t_point			coordinates;
-	t_vector			orientation;
-	int					fov;
-	int					zoom_level;
+	t_vector		orientation;
+	int				fov;
+	int				zoom_level;
 }			t_camera;
 
 typedef struct s_viewport
 {
-	double				width;
-	double				height;
-	double				focal_length;
+	double			width;
+	double			height;
+	double			focal_length;
 	t_point			coordinates;
-	t_vector			orientation;
-//	t_rt_resolution		size;
-	double				x_ratio;
-	double				y_ratio;
+	t_vector		orientation;
+	double			x_ratio;
+	double			y_ratio;
 }			t_viewport;
 
 typedef struct s_scene
@@ -177,37 +176,34 @@ typedef struct s_scene
 	t_obj_union *	objects;
 	t_spot_light *	spot_lights;
 	t_ambient_light	ambient_light;
-	t_camera	*		cameras;
-	t_resolution		canvas;
+	t_camera	*	cameras;
+	t_resolution	canvas;
 	t_viewport		viewport;
 	t_color_ratio	bg_color;
-	int					object_amount;
-	int					light_amount;
-	int					camera_amount;
-	unsigned long		frame_counter;
-	int					thread_amount;
+	int				object_amount;
+	int				light_amount;
+	int				camera_amount;
+	unsigned long	frame_counter;
+	int				thread_amount;
 
-	double				aspect_ratio;
+	double			aspect_ratio;
 
-//	double				red;
-//	double				green;
-//	double				blue;
-	int					recursion_depth;
-	int					msaa;
-	int					hud;
-	bool				bare_toggle;
+	int				recursion_depth;
+	int				msaa;
+	int				hud;
+	bool			bare_toggle;
 }			t_scene;
 
 typedef struct s_mlx
 {
-	mlx_t				*mlx;
-	mlx_image_t			*img;
-	mlx_image_t 		*text;
-	mlx_image_t 		*fps;
-	mlx_image_t 		*rgb;
-	mlx_image_t			*ref;
-	mlx_image_t			*msaa;
-	mlx_image_t			*thread;
+	mlx_t			*mlx;
+	mlx_image_t		*img;
+	mlx_image_t 	*text;
+	mlx_image_t 	*fps;
+	mlx_image_t 	*rgb;
+	mlx_image_t		*ref;
+	mlx_image_t		*msaa;
+	mlx_image_t		*thread;
 }			t_mlx;
 
 typedef struct s_mini_rt
